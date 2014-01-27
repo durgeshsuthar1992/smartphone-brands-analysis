9e6dcaaef91f746ac8dcbb4bffa02a7df563fa81
Durgesh suthar <durgesh.30@rediffmail.com>
2014-01-27 16:27:38 +0530

    refined analysis and added nokia too

--- analyse.php
+++ analyse.php
@@ -4,7 +4,7 @@
 
 	$analyse=new alchemyAPI();
 	$i=0;
-	$sql=$db->query("SELECT tweet_text FROM tweets ORDER BY tweet_id ASC LIMIT 2701,800");
+	$sql=$db->query("SELECT tweet_text FROM tweets ORDER BY tweet_id ASC LIMIT 10001,800");
 	$num=$sql->rowCount();
 	$rows=array();
 	while ($tweet=$sql->fetch(PDO::FETCH_ASSOC)) {
@@ -18,33 +18,33 @@
 					$store1=$db->prepare("INSERT INTO senti (key_name,key_score) VALUES (:name,:score)");
 					if (array_key_exists('score', $keyword['sentiment'])){ 
 						$store1->execute(array(
-							':name'=>'iphone',
+							':name'=>'iPhone',
 							':score'=>$keyword['sentiment']['score']
 							));
 					}
 				}elseif (strpos(strtolower($keyword['text']),'galaxy')>-1||strpos(strtolower($keyword['text']),'samsung')>-1||strpos(strtolower($keyword['text']),'s3')>-1||strpos(strtolower($keyword['text']),'s4')>-1||strpos(strtolower($keyword['text']),'note 3')>-1||strpos(strtolower($keyword['text']),'s duos')>-1) {
 					$store2=$db->prepare("INSERT INTO senti (key_name,key_score) VALUES (:name,:score)");
 					$store2->execute(array(
-						':name'=>'samsung',
+						':name'=>'Samsung',
 						':score'=>$score
 						));
 				}elseif (strpos(strtolower($keyword['text']),'micromax')>-1||strpos(strtolower($keyword['text']),'canvas')>-1) {
 					$store3=$db->prepare("INSERT INTO senti (key_name,key_score) VALUES (:name,:score)");
 					$store3->execute(array(
-						':name'=>'micromax',
+						':name'=>'Micromax',
 						':score'=>$score
 						));
 
 				}elseif (strpos(strtolower($keyword['text']),'xperia')>-1||strpos(strtolower($keyword['text']),'sony')>-1) {
 					$store4=$db->prepare("INSERT INTO senti (key_name,key_score) VALUES (:name,:score)");
 					$store4->execute(array(
-						':name'=>'sony',
+						':name'=>'Sony',
 						':score'=>$score
 						));
 				}elseif (strpos(strtolower($keyword['text']),'one x')>-1||strpos(strtolower($keyword['text']),'htc')>-1) {
 					$store5=$db->prepare("INSERT INTO senti (key_name,key_score) VALUES (:name,:score)");
 					$store5->execute(array(
-						':name'=>'htc',
+						':name'=>'HTC',
 						':score'=>$score
 						));
 				}elseif (strpos(strtolower($keyword['text']),'nexus')>-1) {
@@ -53,10 +53,16 @@
 						':name'=>'Nexus',
 						':score'=>$score
 						));
+				}elseif (strpos(strtolower($keyword['text']),'lumia')>-1||strpos(strtolower($keyword['text']),'nokia')>-1||strpos(strtolower($keyword['text']),'asha')>-1) {
+					$store6=$db->prepare("INSERT INTO senti (key_name,key_score) VALUES (:name,:score)");
+					$store6->execute(array(
+						':name'=>'Nokia',
+						':score'=>$score
+						));
 				}elseif (strpos(strtolower($keyword['text']),'lg')>-1) {
 					$store7=$db->prepare("INSERT INTO senti (key_name,key_score) VALUES (:name,:score)");
 					$store7->execute(array(
-						':name'=>'lg',
+						':name'=>'LG',
 						':score'=>$score
 						));
 				}
